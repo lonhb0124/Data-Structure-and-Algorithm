0cdb4d6b006c781d9aeaf012249b0ec39505f9a3
lonhb0124 <lonhb0124@gmail.com>
2025-06-04 14:55:28 -0400

    edit disjoint and created unique

--- compare_disjoint.py
+++ compare_disjoint.py
@@ -1,13 +1,29 @@
 import random as rand
+from time import time
 
-def disjoint(A, B, C):
 
-    for a in A:
-        for b in B:
-            for c in C:
+""" the worst-case running time of disjoint1 is O(n3) """
+def disjoint1(A, B, C):
+
+    for a in A:                     # O(n) time.
+        for b in B:                 # O(n) time.
+            for c in C:             # O(n) time.
                 if a == b == c:
                     print("The same number between A and B and C is ", a)
                     return False
+    return True
+
+
+""" the worst-case running time of disjoint1 is O(n2) """
+def disjoint2(A, B, C):
+
+    for a in A:                 # O(n) time.
+        for b in B:             # O(n) time.
+            if a == b:          # only check C if a == b
+                for c in C:     # depending on the number of same pair between a and b
+                    if a == c:
+                        print("The same number between A and B and C is ", a)
+                        return False
     return True
 
 if __name__ == '__main__':
@@ -20,14 +36,14 @@ if __name__ == '__main__':
     print(A1)
     print(B1)
     print(C1)
-    print(disjoint(A1, B1, C1))
+    start_time = time()
+    print(disjoint1(A1, B1, C1))
+    end_time = time()
+    elapsed = end_time - start_time
+    print(elapsed)
 
-    n2 = rand.randint(1, 10)
-    A2 = [rand.randint(1,20) for i in range(n2)]
-    B2 = [rand.randint(1, 20) for i in range(n2)]
-    C2 = [rand.randint(1, 20) for i in range(n2)]
-
-    print(A2)
-    print(B2)
-    print(C2)
-    print(disjoint(A2, B2, C2))
+    start_time = time()
+    print(disjoint2(A1, B1, C1))
+    end_time = time()
+    elapsed = end_time - start_time
+    print(elapsed)
---
+++ compare_unique.py
@@ -0,0 +1,45 @@
+import random as rand
+from time import time
+
+
+""" the worst-case running time of disjoint1 is O(n2) """
+def unique1(S):
+
+    for j in range(len(S)):             # O(n) time.
+        for k in range(j+1, len(S)):    # O(n) time.
+            if S[j] == S[k]:
+                print("The duplicate pair in the list is ", S[j])
+                return False            # found duplicate pair
+    return True
+ 
+
+
+""" the worst-case running time of disjoint1 is O(nlog n) """
+def unique2(S):
+
+    temp = sorted(S)                    # create a sorted copy of S, O(nlog n) time.
+    print("Sorted list is ", temp)
+    for j in range(1, len(temp)):             # O(n) time.
+        if temp[j-1] == temp[j]:
+            print("The duplicate pair in the list is ", temp[j-1])
+            return False            # found duplicate pair
+    return True
+
+if __name__ == '__main__':
+    n1 = rand.randint(1, 10)
+    A1 = [rand.randint(1,20) for i in range(n1)]
+
+    print(A1)
+    print("Test unique1")
+    start_time = time()
+    print(unique1(A1))
+    end_time = time()
+    elapsed = end_time - start_time
+    print(elapsed)
+
+    print("Test unique2")
+    start_time = time()
+    print(unique2(A1))
+    end_time = time()
+    elapsed = end_time - start_time
+    print(elapsed)
